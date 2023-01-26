ca639559021f934ea6807e1a6f2518ce714e42e9
Lautaro Pintos <lautipintos8@gmail.com>
2023-01-25 21:54:13 -0300

    version 25/01/2023

--- js/products.js
+++ js/products.js
@@ -4,84 +4,84 @@ const products = [
       "name": "Zapatillas Greca Odissea BLACK",
       "price": 549.99,
       "instock": 13,
-      "imgSrc": "/img/versace-1.avif"
+      "imgSrc": "img/versace-1.avif"
     },
     {
       "id": 1,
       "name": "Zapatillas Greca Odissea WHITE",
       "price": 549.99,
       "instock": 100,
-      "imgSrc": "/img/versace-2.webp"
+      "imgSrc": "img/versace-2.webp"
     },
     {
       "id": 2,
       "name": "Zapatillas Odissea WHITE",
       "price": 719.99,
       "instock": 100,
-      "imgSrc": "/img/versace-3.webp"
+      "imgSrc": "img/versace-3.webp"
     },
     {
       "id": 3,
       "name": "Zapatillas Odissea BLACK",
       "price": 719.99,
       "instock": 100,
-      "imgSrc": "/img/versace-4.webp"
+      "imgSrc": "img/versace-4.webp"
     },
     {
       "id": 4,
       "name": "Zapatillas Greca Odissea ORANGE",
       "price": 719.99,
       "instock": 100,
-      "imgSrc": "/img/versace-5.webp"
+      "imgSrc": "img/versace-5.webp"
     },
     {
       "id": 5,
       "name": "Zapatillas Greca Odissea YELLOW",
       "price": 719.99,
       "instock": 100,
-      "imgSrc": "/img/versace-6.webp"
+      "imgSrc": "img/versace-6.webp"
     },
     {
       "id": 6,
       "name": "Zapatillas Odissea BLACK Con Cristales",
       "price": 1789.99,
       "instock": 100,
-      "imgSrc": "/img/versace-7.webp"
+      "imgSrc": "img/versace-7.webp"
     },
     {
       "id": 7,
       "name": "Zapatillas Odissea WHITE Con Cristales",
       "price": 1789.99,
       "instock": 100,
-      "imgSrc": "/img/versace-8.webp"
+      "imgSrc": "img/versace-8.webp"
     },
     {
       "id": 8,
       "name": "Zapatillas Greca Odissea GOLD",
       "price": 549.99,
       "instock": 100,
-      "imgSrc": "/img/versace-9.avif"
+      "imgSrc": "img/versace-9.avif"
     },
     {
       "id": 9,
       "name": "Zapatillas Greca Odissea SILVER",
       "price": 719.99,
       "instock": 100,
-      "imgSrc": "/img/versace-10.webp"
+      "imgSrc": "img/versace-10.webp"
     },
     {
       "id": 10,
       "name": "Zapatillas Greca Odissea PURPLE",
       "price": 719.99,
       "instock": 100,
-      "imgSrc": "/img/versace-11.webp"
+      "imgSrc": "img/versace-11.webp"
     },
     {
       "id": 11,
       "name": "Zapatillas Greca Odissea FOREST GREEN",
       "price": 719.99,
       "instock": 100,
-      "imgSrc": "/img/versace-12.webp"
+      "imgSrc": "img/versace-12.webp"
     }
   ]
 
@@ -91,55 +91,55 @@ const products = [
       "name": "Chaqueta bomber de nailon Varsity",
       "price": 1899.99,
       "instock": 13,
-      "imgSrc": "/img/Jackets/jacket-1.webp"
+      "imgSrc": "img/Jackets/jacket-1.webp"
     },
     {
       "id": 16,
       "name": "Chaqueta bomber de piel Greca",
       "price": 2549.99,
       "instock": 13,
-      "imgSrc": "/img/Jackets/jacket-2.webp"
+      "imgSrc": "img/Jackets/jacket-2.webp"
     },
     {
       "id": 17,
       "name": "Chaqueta cortavientos Barocco 660",
       "price": 3249.99,
       "instock": 13,
-      "imgSrc": "/img/Jackets/jacket-3.webp"
+      "imgSrc": "img/Jackets/jacket-3.webp"
     },
     {
       "id": 18,
       "name": "Chaqueta bomber reversible Barocco 660",
       "price": 1449.99,
       "instock": 13,
-      "imgSrc": "/img/Jackets/jacket-4.webp"
+      "imgSrc": "img/Jackets/jacket-4.webp"
     },
     {
       "id": 19,
       "name": "Chaqueta bomber acolchada Barocco 660",
       "price": 1549.99,
       "instock": 13,
-      "imgSrc": "/img/Jackets/jacket-5.webp"
+      "imgSrc": "img/Jackets/jacket-5.webp"
     },
     {
       "id": 20,
       "name": "Cazadora bomber de nailon con logotipo",
       "price": 2989.99,
       "instock": 13,
-      "imgSrc": "/img/Jackets/jacket-6.webp"
+      "imgSrc": "img/Jackets/jacket-6.webp"
     },
     {
       "id": 21,
       "name": "Chaqueta bomber del Festival de Primavera",
       "price": 2229.99,
       "instock": 13,
-      "imgSrc": "/img/Jackets/jacket-7.webp"
+      "imgSrc": "img/Jackets/jacket-7.webp"
     },
     {
       "id": 22,
       "name": "Chaqueta bomber reversible La Greca",
       "price": 1989.99,
       "instock": 13,
-      "imgSrc": "/img/Jackets/jacket-8.webp"
+      "imgSrc": "img/Jackets/jacket-8.webp"
     },
   ]
